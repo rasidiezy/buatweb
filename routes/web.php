fb9c09e5c674fcc4412d8abb6837408421ba1722
Your Name <rasidi751@gmail.com>
2022-02-15 18:32:57 +0700

    slicing landing page, checkout page, login page, success checkout.

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/checkouts', function () {
+    return view('checkouts');
+})->name('checkouts');
+
+Route::get('/success-checkouts', function () {
+    return view('success_checkouts');
+})->name('success-checkout');
